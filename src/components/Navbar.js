bd345e5a47cffbcfb37c86135fc500be862df6ce
gavin33361 <gzheng0214@gmail.com>
2021-02-02 04:02:59 -0500

    Fixed hamburger button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@
  * @Author: Gavin
  * @Date:   2021-01-30 01:44:33
  * @Last Modified by:   Gavin
- * @Last Modified time: 2021-02-01 19:00:53
+ * @Last Modified time: 2021-02-02 03:50:44
  */
 import React, {useState, useEffect}from 'react';
 import { ReactComponent as Logo } from '../images/faceIcon.svg';
@@ -45,8 +45,6 @@ const Navbar = () => {
 			</li>
 		</ul>
 		<div className="navbar__hamburger" onClick={() => setOpen(!open)}>
-			<span className={menuClassName}></span>
-			<span className={menuClassName}></span>
 			<span className={menuClassName}></span>
 		</div>
 		</nav>
